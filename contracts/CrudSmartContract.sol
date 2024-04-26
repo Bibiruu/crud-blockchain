3e7396aed2eaededf995a2abe018111b0e228390
Maria <so.mariagloria@gmail.com>
2024-04-27 01:12:13 +0300

    testing crud functions

--- contracts/CrudSmartContract.sol
+++ contracts/CrudSmartContract.sol
@@ -8,34 +8,56 @@ contract CrudSmartContract {
     }
     //array of users
     User[] public users;
-    uint public nextId;
+    uint public nextId = 1;
 
     function create(string memory name) public {
         users.push(User(nextId, name));
-        //option 2. when array is having too many fields: users.push(User({nextId: nextId, name: name}))
+        //option 2. when array is having too many fields(key-value pair): users.push(User({nextId: nextId, name: name}))
         //needing this to add new id user
         nextId++;
     }
 
-    function read(uint id) view public returns(uint, string memory) {
-     for (uint i = 0; i < users.length; i++) {
-        if (users[i].id == id) {
-            return(users[i].id, users[i].name);
-        }
+    function read(uint id) public view returns (uint, string memory) {
+        //find the position in the user array
+        uint i = find(id);
+        return (users[i].id, users[i].name);
     }
-    revert("User not found");
-}
-  
 
     function update(uint id, string memory name) public {
-        for (uint i = 0; i < users.length; i++) {
-            if (users[i].id == id) {
-                users[i].name = name;
-            }
-        }
+        uint i = find(id);
+        users[i].name = name;
     }
 
     function deleteId(uint id) public {
-        delete users[id];
+        uint i = find(id);
+        delete users[i];
+    }
+
+    //position of the user struct in the users array
+    function find(uint id) internal view returns (uint) {
+        for (uint i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                return i;
+            }
+        }
+        revert("User doesnt exist");
+    }
+
+    function showAll() public view returns (string memory) {
+        string memory allNames;
+
+        for (uint i = 0; i < users.length; i++) {
+            if (bytes(users[i].name).length > 0) {
+                // Check if the name is not empty
+                if (bytes(allNames).length > 0) {
+                    allNames = string(
+                        abi.encodePacked(allNames, ", ", users[i].name)
+                    );
+                } else {
+                    allNames = users[i].name;
+                }
+            }
+        }
+        return allNames;
     }
 }
--- tests/CrudSmartContract.test.js
+++ tests/CrudSmartContract.test.js
@@ -1,1 +1,35 @@
-const CrudSmartContract = require('CrudSmartContract')
+const CrudSmartContract = artifacts.require('CrudSmartContract')
+
+contract("CrudSmartContract", () => {
+    let crudSmartContract = null;
+
+    before(async () => {
+        crudSmartContract = await CrudSmartContract.deployed()
+    });
+    it('should create a new user', async () => {
+        await crudSmartContract.create('Joseph');
+        //read() also tested
+        const user = await crudSmartContract.read(1);
+        assert(user[0].toNumber() === 1);
+        assert(user[1] === 'Joseph');
+    });
+    it('should update user', async () => {
+        await crudSmartContract.update(1, 'Jose');
+        const user = await crudSmartContract.read(1);
+        assert(user[0].toNumber() === 1);
+        assert(user[1] === 'Jose');
+    });
+    //revert handling
+    it('should not update a non-existing user', async () => {
+        try {
+            await crudSmartContract.update(2, 'Jo');
+        }
+        catch (error) {
+            assert(error.message.includes('User doesnt exist'));
+            return;
+        }
+        
+        assert(false);
+    });
+
+});
