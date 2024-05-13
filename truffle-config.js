9aa477d1920de07ae75723ad0ce37aad34385fc9
Maria <so.mariagloria@gmail.com>
2024-05-14 00:36:30 +0300

    optimized configuration for infura requests

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,10 @@
-/**
- * Hands-off deployment with Infura
- * --------------------------------
- *
- * Do you have a complex application that requires lots of transactions to deploy?
- * Use this approach to make deployment a breeze 🏖️:
- *
- * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
- * to sign transactions before they're sent to a remote public node.
- * Infura accounts are available for free at 🔍: https://infura.io/register
- *
- * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file.
- * In your project root, run `$ npm install dotenv`.
- * Create .env (which should be .gitignored) and declare your MNEMONIC
- * and Infura PROJECT_ID variables inside.
- * For example, your .env file will have the following structure:
- *
- * MNEMONIC = <Your 12 phrase mnemonic>
- * PROJECT_ID = <Your Infura project id>
- */
-
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
+// Exponential backoff formula: 2^attempt * 1000 (milliseconds)
+const exponentialBackoff = (attempt) => {
+  return Math.pow(2, attempt) * 1000;
+};
 
 module.exports = {
   networks: {
@@ -35,44 +17,26 @@ module.exports = {
     sepolia: {
       provider: () => new HDWalletProvider(
         process.env.MNEMONIC, 
-        `https://sepolia.infura.io/v3/${process.env.INFURA_NODE_ENDPOINT}`
+        `https://sepolia.infura.io/v3/${process.env.INFURA_NODE_ENDPOINT}`, {
+        //connection to be reused for multiple requests, rather than establishing a new oe
+        providerOptions: {
+          keepAlive: true,
+          //time out to 20sec,prevent from waiting indefinitely
+          timeout: 20000,
+        },
+        clientConfig: {
+          maxAttempts: 5,
+          //Function to calculate retry interval
+          interval: exponentialBackoff
+        },
+        }
       ),
       network_id: 11155111
     }
   },
-
   compilers: {
     solc: {
-      version: "0.8.19",      // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "0.8.19",  
     }
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  //   enabled: false,
-  //   host: "127.0.0.1",
-  //   adapter: {
-  //     name: "indexeddb",
-  //     settings: {
-  //       directory: ".db"
-  //     }
-  //   }
-  // }
 };
